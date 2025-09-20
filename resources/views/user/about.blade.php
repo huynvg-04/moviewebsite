8675956068684c702238c8e635392b1cabe708cf
Huy <88840321+cornhuy04@users.noreply.github.com>
2025-09-20 20:50:21 +0700

    Add files via upload

---
+++ resources/views/user/about.blade.php
@@ -0,0 +1,24 @@
+@extends('user.master')
+@section('title','Giới Thiệu Về Chúng Tôi - 12C4Movies')
+@section('content')
+<div class="faq">
+    <h4 class="latest-text w3_faq_latest_text w3_latest_text">Giới Thiệu</h4>
+    <div class="container">
+        <div class="agileits-news-top">
+            <ol class="breadcrumb">
+                <li><a href="{{ route('user.index') }}">Trang Chủ</a></li>
+                <li class="active">Giới Thiệu</li>
+            </ol>
+            <div>
+                <h4>Chào mừng bạn đến với 12C4Movies – nền tảng xem phim trực tuyến hàng đầu dành cho mọi tín đồ yêu thích phim ảnh! Chúng tôi tự hào là nơi mang đến trải nghiệm giải trí tuyệt vời với kho tàng phim đa dạng, từ các bộ phim bom tấn Hollywood, phim hành động kịch tính, đến các bộ phim tình cảm sâu lắng và các bộ phim hoạt hình vui nhộn.<br><br></h4>
+            </div>
+        </div>
+    </div>
+    <h4 class="latest-text w3_faq_latest_text w3_latest_text">Liên Hệ</h4>
+    <div class="container">
+        <h4 class="pt-1"><b>Số điện thoại: </b>0999999999</h4>
+        <h4><b>Facebook: </b>https://facebook.com/profile.php?=75548495</h4>
+        <h4><b>Email: </b>22111061@hunre.edu.vn</h4>
+    </div>
+</div>
+@endsection
